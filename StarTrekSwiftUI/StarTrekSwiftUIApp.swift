7a9975ecef52c62bd156b73e0349183fb07d0c92
Kevin Lynch <klynch71@gmail.com>
2025-05-10 15:49:11 -0500

    Initial Commit

---
+++ StarTrekSwiftUI/StarTrekSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarTrekSwiftUIApp.swift
+//  StarTrekSwiftUI
+//
+//  Created by Kevin Lynch on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StarTrekSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarTrekSwiftUITests/StarTrekSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  StarTrekSwiftUITests.swift
+//  StarTrekSwiftUITests
+//
+//  Created by Kevin Lynch on 5/10/25.
+//
+
+import Testing
+@testable import StarTrekSwiftUI
+
+struct StarTrekSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
